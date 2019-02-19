ce632a582b4512ea3d0593c585b0148f77d85855
Effsy <jardinomidvaran@hotmail.co.uk>
2019-02-19 17:35:56 +0000

    Initial setup

---
+++ migrations/2_DEV_migrate_dependencies.js
@@ -0,0 +1,21 @@
+/* global artifacts */
+/* eslint no-undef: "error" */
+
+// migrateDx
+//  * Is a migration script made for deploying DutchX in a local-ganache test
+//    environment in a simpler way
+//  * For more details check out:
+//    https://github.com/gnosis/dx-contracts/blob/master/src/migrations-truffle-5/index.js
+const migrateDx = require('@gnosis.pm/dx-contracts/src/migrations-truffle-5')
+
+module.exports = function (deployer, network, accounts) {
+  return migrateDx({
+    artifacts,
+    deployer,
+    network,
+    accounts,
+    web3,
+    thresholdNewTokenPairUsd: process.env.THRESHOLD_NEW_TOKEN_PAIR_USD,
+    thresholdAuctionStartUsd: process.env.THRESHOLD_AUCTION_START_USD
+  })
+}
---
+++ truffle-config.js
@@ -0,0 +1,68 @@
+const HDWalletProvider = require('truffle-hdwallet-provider')
+const assert = require('assert')
+
+const DEFAULT_GAS_PRICE_GWEI = 5
+const GAS_LIMIT = 6.5e6
+const DEFAULT_MNEMONIC = 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat'
+
+function truffleConfig ({
+  mnemonic = DEFAULT_MNEMONIC,
+  gasPriceGWei = process.env.GAS_PRICE_GWEI || DEFAULT_GAS_PRICE_GWEI,
+  gas = GAS_LIMIT,
+  optimizedEnabled = true,
+  urlRinkeby = 'https://rinkeby.infura.io/',
+  urlMainnet = 'https://mainnet.infura.io',
+  urlDevelopment = 'localhost',
+  portDevelopment = 8545,
+  solcVersion = '0.5.2',
+  solcUseDocker = false
+} = {}) {
+  assert(mnemonic, 'The mnemonic has not been provided')
+  console.log(`Using gas limit: ${gas / 1000} K`)
+  console.log(`Using gas price: ${gasPriceGWei} Gwei`)
+  console.log(`Optimizer enabled: ${optimizedEnabled}`)
+  console.log('Using default mnemonic: %s', mnemonic === DEFAULT_MNEMONIC)
+  const gasPrice = gasPriceGWei * 1e9
+
+  const _getProvider = url => {
+    return () => new HDWalletProvider(mnemonic, url)
+  }
+
+  return {
+    networks: {
+      development: {
+        host: urlDevelopment,
+        port: portDevelopment,
+        gas,
+        gasPrice,
+        network_id: '*'
+      },
+      mainnet: {
+        provider: _getProvider(urlMainnet),
+        network_id: '1',
+        gas,
+        gasPrice
+      },
+      rinkeby: {
+        provider: _getProvider(urlRinkeby),
+        network_id: '4',
+        gas,
+        gasPrice
+      }
+    },
+    compilers: {
+      solc: {
+        version: solcVersion,
+        docker: solcUseDocker,
+        settings: {
+          optimizer: {
+            enabled: optimizedEnabled,
+            runs: 200
+          }
+        }
+      }
+    }
+  }
+}
+
+module.exports = truffleConfig()
