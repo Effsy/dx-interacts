3f9ac49fd061cd069f12dfe6ab07e3e5e36d9e0d
Frederico A Lacs <fredlacs@gmail.com>
2019-02-25 09:54:07 +0000

    Read from dx as a price feed

--- contracts/dxInteracts.sol
+++ contracts/dxInteracts.sol
@@ -11,11 +11,27 @@ contract dxInteracts {
     }
 
     // TODO: only owner
-    function updateDx(address _dx) public {
+    function _updateDx(address _dx) public {
         require(address(_dx) != address(0), "DutchExchange can't have address 0");
         dx = DutchExchange(_dx);  
     }
 
 
+    // 2. We create the function that uses DutchX as a price feed
+    function getBalanceInUsd (address token) public view returns (uint, uint) {
+        uint pricetNum;
+        uint priceDen;
+        // Get the price in ETH for a token
+        (pricetNum, priceDen) = dx.getPriceOfTokenInLastAuction(token);
 
+        // Get the price of ETH
+        PriceOracleInterface priceOracle = PriceOracleInterface(dx.ethUSDOracle());
+        uint etherUsdPrice = priceOracle.getUSDETHPrice();
+        // uint etherUsdPrice = 400 ether;
+
+        // Return the price in USD:
+        //    balance * Price TOKEN-ETH * price ETH-USD
+        uint balance = balances[token];
+        return (balance * pricetNum * etherUsdPrice, priceDen);
+    }
 }
