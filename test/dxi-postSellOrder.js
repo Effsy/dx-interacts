b9eafea20edae93c64b51a16f01fb1d2a60bfad9
Frederico A Lacs <fredlacs@gmail.com>
2019-03-02 15:52:12 +0000

    Add necessary variables to interact with dutchExchange for tests

--- test/dxi-postSellOrder.js
+++ test/dxi-postSellOrder.js
@@ -1,6 +1,21 @@
 const dxInteracts = artifacts.require("dxInteracts");
+const { getContracts, setupTest, wait } = require("@gnosis.pm/dx-contracts/test/testFunctions")
 
 contract("dxInteracts", async accounts => {
+
+    before(
+        async () => {
+            contracts = await getContracts();
+            // destructure contracts into easy to use aliases
+            ({
+                EtherToken: eth,
+                TokenGNO: gno,
+                TokenFRT: mgn,
+                DutchExchange: dx
+            } = contracts)
+        }
+    )
+
     it("should post sell order on dutchExchange", async () => {
         let dxi = await dxInteracts.deployed();
 
