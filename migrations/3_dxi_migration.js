9822a3b7f6aa7ee849c9da769d6432a630fc58c1
Frederico A Lacs <fredlacs@gmail.com>
2019-03-02 15:08:43 +0000

    Refactor include semicolon at end of lines

--- migrations/3_dxi_migration.js
+++ migrations/3_dxi_migration.js
@@ -1,11 +1,11 @@
 const dxInteracts = artifacts.require("./dxInteracts.sol");
-const DutchExchangeProxy = artifacts.require("DutchExchangeProxy")
+const DutchExchangeProxy = artifacts.require("DutchExchangeProxy");
 
 module.exports = async (deployer, network, accounts) => {
 
-    const dxProxy = await DutchExchangeProxy.deployed()
-    console.log("Deploying dxInteracts with %s as the DutchExchange contract", dxProxy.address)
+    const dxProxy = await DutchExchangeProxy.deployed();
+    console.log("Deploying dxInteracts with %s as the DutchExchange contract", dxProxy.address);
 
-    const dxi = await deployer.deploy(dxInteracts, dxProxy.address)
-    console.log("dxi deployed at %s", dxi.address)
+    const dxi = await deployer.deploy(dxInteracts, dxProxy.address);
+    console.log("dxi deployed at %s", dxi.address);
 }
