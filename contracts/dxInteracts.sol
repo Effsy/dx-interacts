840bc3a1211acd7100ea8bdd853db7c52b9a23e9
Frederico A Lacs <fredlacs@gmail.com>
2019-03-03 18:41:58 +0000

    Add weth call to buy tokens in dutchX

--- contracts/dxInteracts.sol
+++ contracts/dxInteracts.sol
@@ -1,8 +1,20 @@
 pragma solidity ^0.5.2;
 
 import "./DxInterface.sol";
-
+import "./Weth.sol";
 
 contract dxInteracts is DxInterface {
-    constructor(address _dx) DxInterface(_dx) public {}
+
+    WETH public ethToken;
+
+    constructor(address _dx, address _ethToken) DxInterface(_dx) public {
+        require(_ethToken != address(0), "The WETH address must be valid");
+        ethToken = WETH(_ethToken);
+    }
+
+    function sellEther(address buyToken) external payable  {
+        ethToken.deposit.value(msg.value)();
+        ethToken.approve(address(dx), msg.value);
+        DxInterface.depositAndSell(address(ethToken), buyToken, msg.value);
+    }
 }
